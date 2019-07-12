11342392b93ae6add83ed520d9cc96bd22ebb75e
mirufu <hi@rshier.com>
2019-07-12 14:36:27 +0700

    Add practice model

---
+++ zayli/Views/Controllers/PracticeModel.swift
@@ -0,0 +1,23 @@
+//
+//  PracticeModel.swift
+//  zayli
+//
+//  Created by rshier on 12/07/19.
+//  Copyright © 2019 rshier. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class Practice: Object {
+    @objc dynamic var id = UUID().uuidString
+    @objc dynamic var timestamp  = NSDate().timeIntervalSince1970
+    @objc dynamic var idea = ""
+    @objc dynamic var target = ""
+    @objc dynamic var summary = ""
+    @objc dynamic var background = ""
+
+    override static func primaryKey() -> String? {
+        return "id"
+    }
+}
