8d0c0f486be1b243467f508e46198cdd6f2a9620
mirufu <hi@rshier.com>
2019-07-12 15:20:57 +0700

    Add Eureka extension

---
+++ zayli/Extensions/EurekaExtension.swift
@@ -0,0 +1,16 @@
+//
+//  Eureka.swift
+//  zayli
+//
+//  Created by rshier on 12/07/19.
+//  Copyright © 2019 rshier. All rights reserved.
+//
+
+import Foundation
+import Eureka
+
+extension Form {
+    func getMultivaluedSection(_ key: String) -> Array<Any>{
+        return (self.values()[key]!! as! [Any?]).compactMap { $0 }
+    }
+}
