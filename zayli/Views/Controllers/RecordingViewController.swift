9bcfd81abf6014afb89fb8768cb569cbe42daafd
mirufu <hi@rshier.com>
2019-07-16 13:40:00 +0700

    Add recording page

---
+++ zayli/Views/Controllers/RecordingViewController.swift
@@ -0,0 +1,17 @@
+//
+//  RecordingViewController.swift
+//  zayli
+//
+//  Created by rshier on 16/07/19.
+//  Copyright © 2019 rshier. All rights reserved.
+//
+
+import UIKit
+
+class RecordingViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
