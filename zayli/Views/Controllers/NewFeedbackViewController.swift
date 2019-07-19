5ea3eba233a2b044352271be24ac36188aadaccd
mirufu <hi@rshier.com>
2019-07-19 19:19:46 +0700

    Update button font weight

--- zayli/Views/Controllers/NewFeedbackViewController.swift
+++ zayli/Views/Controllers/NewFeedbackViewController.swift
@@ -12,6 +12,8 @@ import RealmSwift
 
 class NewFeedbackViewController: FormViewController {
     
+    @IBOutlet weak var doneButton: UIBarButtonItem!
+    
     @IBAction func cancelAction(_ sender: UIBarButtonItem) {
         self.presentingViewController?.dismiss(animated: true, completion: nil)
     }
@@ -40,33 +42,89 @@ class NewFeedbackViewController: FormViewController {
         setupForm()
     }
     
+    func formValidate(){
+        var errorCount = 0
+        
+        form.allRows.flatMap { row in
+            let error = row.validate()
+            
+            if !error.isEmpty {
+                errorCount += 1
+            }
+        }
+        
+        if errorCount == 0 {
+            doneButton.isEnabled = true
+        }
+    }
+    
     func setupForm(){
         tableView.isEditing = true
+        doneButton.isEnabled = false
+        
+        var rules = RuleSet<String>()
+        rules.add(rule: RuleRequired())
+        rules.add(rule: RuleMinLength(minLength: 3))
         
         form
             +++ Section("name")
             <<< TextRow("name"){ row in
                 row.placeholder = "What is your name?"
+                
+                row.add(ruleSet: rules)
+                row.validationOptions = .validatesOnDemand
+                
+                row.onChange { _ in
+                    self.formValidate()
+                }
             }
             
             +++ Section("idea")
             <<< TextRow("idea"){ row in
                 row.placeholder = "What idea have you get?"
+                
+                row.add(ruleSet: rules)
+                row.validationOptions = .validatesOnDemand
+                
+                row.onChange { _ in
+                    self.formValidate()
+                }
             }
             
             +++ Section("understanding")
             <<< TextAreaRow("understanding"){ row in
                 row.placeholder = "Your understanding about the idea"
+                
+                row.add(ruleSet: rules)
+                row.validationOptions = .validatesOnDemand
+                
+                row.onChange { _ in
+                    self.formValidate()
+                }
             }
                     
             +++ Section("opinion")
             <<< TextAreaRow("opinion"){ row in
                 row.placeholder = "Your opinion about story delivering"
+                
+                row.add(ruleSet: rules)
+                row.validationOptions = .validatesOnDemand
+                
+                row.onChange { _ in
+                    self.formValidate()
+                }
             }
                     
             +++ Section("emotion")
             <<< TextRow("emotion"){ row in
                 row.placeholder = "What kind of emotion do you feel?"
+                
+                row.add(ruleSet: rules)
+                row.validationOptions = .validatesOnDemand
+                
+                row.onChange { _ in
+                    self.formValidate()
+                }
             }
         
         }
